b3982700f89e4601c9fb6eacbec11dd8b268df28
Kevin Camacena <kevincamacenabenoit@gmail.com>
2020-02-03 16:29:23 -0600

    Archivo cabecera de la librería numero a texto
    Define las 4 funciones que podremos invocar para traducir números.
    void convierte_numero_a_texto_hasta_99(char*, int*);
    void convierte_numero_a_texto_hasta_999(char*, int*);
    void convierte_numero_a_texto_hasta_99999(char*, int*);
    void convierte_numero_a_texto_hasta_999999(char*,int*);

---
+++ numero_a_texto.h
@@ -0,0 +1,11 @@
+#ifndef __NUMERO_A_TEXTO__
+#define __NUMERO_A_TEXTO__
+#endif
+
+#include <string.h>
+#include <stdlib.h>
+
+void convierte_numero_a_texto_hasta_99(char*, int*);
+void convierte_numero_a_texto_hasta_999(char*, int*);
+void convierte_numero_a_texto_hasta_99999(char*, int*);
+void convierte_numero_a_texto_hasta_999999(char*,int*);
